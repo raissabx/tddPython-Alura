fe439681bb091ce0178605547eb06d72a9722d16
rai_xavier <raissa.xavier@luizalabs.com>
2023-04-25 14:57:52 -0300

    first commit

---
+++ tests/test_bytebank.py
@@ -0,0 +1,20 @@
+from bytebank import Funcionario
+
+class TestClass:
+    def test_quando_idade_recebe_13_03_2000_deve_retornar_22(self):
+        entrada = '13/03/2000' # Given-Contexto
+        esperado = 22
+
+        funcionario_teste = Funcionario('Teste', entrada, 1111)
+        resultado = funcionario_teste.idade() # When-ação
+
+        assert resultado == esperado  # Then-desfecho
+
+    def test_quando_sobrenome_recebe_Lucas_Carvalho_deve_retornar_Carvalho(self):
+        entrada = ' Lucas Carvalho ' # Given
+        esperado = 'Carvalho'
+
+        lucas = Funcionario(entrada, '11/11/2000', 1111)
+        resultado = lucas.sobrenome() # When
+
+        assert resultado == esperado
